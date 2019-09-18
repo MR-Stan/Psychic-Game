0c75e1eb750e5792fc7d1f584c6fe54f8eb0c367
MR-Stan <michaelrstanley87@gmail.com>
2019-09-18 13:52:35 -0500

    completed assignment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,80 @@
+// dom
+const guessNumText = document.getElementById("guessnum");
+const lossText = document.getElementById("losses");
+const winText = document.getElementById("wins");
+const guessText = document.getElementById("guesses");
 
-// initializing variables
-var userWin = 0;
-var userLoss = 0;
-var guessesRem = 9;
-var guesses = [];
-
+// computer variables
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// choosing random letter from array
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-// running function on key stroke
-document.onkeyup = function (event) {
-    // determining which key
-    var input = event.key;
-    var userGuess = input.toLowerCase();
-    var winText = document.getElementById("wins");
-    var lossText = document.getElementById("losses");
-    var guessNumText = document.getElementById("guessnum");
-    var guessText = document.getElementById("guesses");
+// user variables
+var guesses = [];
+var guessesRem = 9;
+var userLoss = 0;
+var userWin = 0;
+var input;
+var userGuess;
 
-    if (guesses.includes(input)) {
-        alert("You've already picked " + input + ". Please try again." );
-    }
-    else {
-        guesses.push(input);
+// resets game
+var reset = function() {
+    guessesRem = 9;
+    guesses = [];
+    guessNumText.innerHTML = guessesRem;
+    guessText.innerHTML = guesses;
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+// when a key is pressed
+document.onkeyup = function (event) {
+    input = event.key;
+    userGuess = input.toLowerCase(); 
+
+    // verifying input is letter
+    if (computerChoices.includes(input)) {
+
+        // verifying input is not a duplicate
+        if (! guesses.includes(input)) {
+        guesses.push(userGuess); // keeping track of what letters have been used
         guessText.innerHTML = guesses;
-        if (guessesRem > 0) {
-            if (userGuess == computerGuess) {
+
+            // if final guess doesn't match
+            if (guessesRem === 1 && userGuess !== computerGuess) {
+                alert("'" + userGuess + "' is incorrect. You're out of guesses, you lose!")
+                userLoss++;
+                lossText.innerHTML = userLoss;
+
+                // resetting game
+                reset();
+            }
+
+            // if input matches
+            else if (userGuess === computerGuess) {
                 alert("'" + userGuess + "' was my letter. You WIN!");
                 userWin++;
                 winText.innerHTML = userWin;
-// initialize
+
+                // resetting game
+                reset();
             }
+
+            // validation criteria met but guess doesn't match
             else {
                 guessesRem--;
-                alert("'" + userGuess + "' is incorrect. You have " + guessesRem + " guesses remaining.");
                 guessNumText.innerHTML = guessesRem;
             }
-        }
+        } 
+
+        // if duplicate entry
         else {
-            alert("You're out of guesses, you lose!");
-            userLoss++;
-            lossText.innerHTML = userLoss;
-// initialize
+            alert("You've already used '" + userGuess + "'. Try again.");
         }
     }
+
+    // if entry is not a letter
+    else {
+        alert("Invalid entry. Enter a letter.");
+    }
 }
+
+
